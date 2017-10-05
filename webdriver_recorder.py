cbe39403749fe6d38d50ac820b0f384fe1fd6f76
Jeff Franklin <jpf@uw.edu>
2017-10-05 09:33:51 -0700

    Scale the screenshots down.

--- webdriver_recorder.py
+++ webdriver_recorder.py
@@ -95,25 +95,28 @@ def browser():
 @pytest.fixture(scope='session')
 def report_file():
     """Open file webdriver-report.html during our test runs."""
-    statics = """
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
-    """
     starttime = datetime.datetime.now()
     with open('webdriver-report.html', mode='w') as fd:
-        fd.write(f"""
+        fd.write("""
         <!DOCTYPE html>
         <html>
             <head>
                 <title>Identity Signup Storyboard</title>
-                {statics}
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+                <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+                <style>
+                  img {
+                    vertical-align: text-top;
+                    width: 250px;
+                  }
+                </style>
             </head>
             <body>
                 <h1>Results for Identity Signup Scenarios</h1>
-                <p>Started {starttime}
         """)
+        fd.write(f'<p>Started {starttime}</p>')
         yield fd
         fd.write('</body></html>')
 
@@ -125,18 +128,18 @@ def report_test(report_file, request, browser):
     Import this into test files that use the browser.
     """
     yield
+
     report_file.write(f'<h2>{request.node.report_call.report.nodeid}</h2>')
     pngs = browser.pngs
     browser.pngs = []
     if request.node.report_call.doc:
-        report_file.write(f'<p>{request.node.report_call.doc}</p>')
+        report_file.write(f'<h3>{request.node.report_call.doc}</p>')
     if request.node.report_call.report.failed:
         report_file.write('<div class="alert alert-danger">{}</div>'.format(
             request.node.report_call.excinfo))
-    report_file.write('<div style="white-space: nowrap">\n')
+    report_file.write('<div class="text-nowrap">\n')
     for png in pngs:
-        report_file.write(f'<img style="vertical-align: text-top;"'
-                          f' src="data:image/png;base64,{png}">\n')
+        report_file.write(f'<img src="data:image/png;base64,{png}">\n')
     report_file.write('</div>\n')
 
 
