54eca78cbb7ee6a955a12ddf27386302b9a69b05
Jeff Franklin <jpf@uw.edu>
2018-09-27 13:09:27 -0700

    Fix setup.py py_modules. Add tests.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name='webdriver-recorder',
       install_requires=['selenium', 'pytest'],
-      version='0.3',
+      version='0.4',
       description=(
           'Enhances a selenium webdriver to record screenshots along the way'),
-      py_modules='webdriver_recorder',
+      py_modules=['webdriver_recorder'],
+      setup_requires=['pytest-runner'],
       entry_points={'pytest11': [
           'webdriver_recorder = webdriver_recorder']}
       )
