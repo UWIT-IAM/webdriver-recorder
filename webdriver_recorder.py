4eba541df49aa674d9ac8ebc4e056a1147629a41
Jeff Franklin <jpf@uw.edu>
2018-11-16 16:40:25 -0800

    Use node_modules/.bin/phantomjs only if it's there.

--- webdriver_recorder.py
+++ webdriver_recorder.py
@@ -11,6 +11,7 @@ report_file - a fixture for handling the setup and teardown of the webdriver
    report. This is currently hardwired to write to webdriver-report.html
 report_test - a fixture for reporting on an individual test run.
 """
+import os
 import types
 import pytest
 import datetime
@@ -28,135 +29,135 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-@contextmanager
-def get_browser(
-        *args, driver=webdriver.PhantomJS,
-        default_width=400, default_height=200, **kwargs):
-    """Return a browser context of type driver."""
-    class BrowserRecorder(driver):
+class BrowserRecorder(webdriver.PhantomJS):
+    """
+    A selenium webdriver with some extra convenience utilities and some
+    automatic screenshot capturing.
+    """
+    def __init__(self, *args, width=400, height=200, **kwargs):
+        project_phantomjs = os.path.join('node_modules', '.bin', 'phantomjs')
+        if not args and os.path.isfile(project_phantomjs):
+            # First arg is path to phantomjs. See if it's in node_modules/.
+            args = (project_phantomjs,)
+
+        super().__init__(*args, **kwargs)
+        self.set_window_size(width=width, height=height)
+        self.pngs = []  # where to store the screenshots
+        # optionally set by the caller for decryption in send_secret
+        self.autocapture = True   # automatically capture screenshots
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.quit()
+
+    @contextmanager
+    def autocapture_off(self):
+        """Context manager temporarily disabling automatic screenshot generation."""
+        previous_autocapture = self.autocapture  # for nesting
+        self.autocapture = False
+        try:
+            yield
+        finally:
+            self.autocapture = previous_autocapture
+
+    def clear(self):
+        """Clear the active element."""
+        self.switch_to.active_element.clear()
+
+    def click(self, tag, substring='', wait=True, timeout=5, capture_delay=0):
         """
-        A selenium webdriver with some extra convenience utilities and some
-        automatic screenshot capturing.
+        Find tag containing substring and click it.
+        wait - give it time to show up in the DOM.
         """
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.set_window_size(width=default_width, height=default_height)
-            self.pngs = []  # where to store the screenshots
-            # optionally set by the caller for decryption in send_secret
-            self.autocapture = True   # automatically capture screenshots
-
-        @contextmanager
-        def autocapture_off(self):
-            """Context manager temporarily disabling automatic screenshot generation."""
-            previous_autocapture = self.autocapture  # for nesting
-            self.autocapture = False
-            try:
-                yield
-            finally:
-                self.autocapture = previous_autocapture
-
-        def clear(self):
-            """Clear the active element."""
-            self.switch_to.active_element.clear()
-
-        def click(self, tag, substring='', wait=True, timeout=5, capture_delay=0):
-            """
-            Find tag containing substring and click it.
-            wait - give it time to show up in the DOM.
-            """
-            search = (By.XPATH, xpath_contains(f'//{tag}', substring))
-            with self.wrap_exception(f'find tag "{tag}" with string "{substring}"'):
-                if wait and timeout:
-                    wait = Waiter(self, timeout)
-                    wait.until(EC.element_to_be_clickable(search),
-                               capture_delay=capture_delay)
-                self.find_element(*search).click()
-
-        def click_button(self, substring=''):
-            """
-            Wait for a button with substring to become clickable then click it.
-            """
-            search = (By.XPATH, xpath_contains(f'//button', substring))
-            with self.wrap_exception(f'click button with string "{substring}" when clickable'):
-                wait = Waiter(self, getattr(self, 'default_wait', 5))
-                wait.until(EC.element_to_be_clickable(search))
-                self.find_element(*search).click()
-
-        def wait_for(self, tag, substring, timeout=None, capture_delay=0):
-            """Wait for tag containing substring to show up in the DOM."""
-            if timeout is None:
-                timeout = getattr(self, 'default_wait', 5)
-            search = (By.XPATH, xpath_contains(f'//{tag}', substring))
-            with self.wrap_exception(f'wait for visibility of tag "{tag}" with string "{substring}"'):
+        search = (By.XPATH, xpath_contains(f'//{tag}', substring))
+        with self.wrap_exception(f'find tag "{tag}" with string "{substring}"'):
+            if wait and timeout:
                 wait = Waiter(self, timeout)
-                wait.until(EC.visibility_of_element_located(search),
-                           capture_delay=capture_delay)
+                wait.until(EC.element_to_be_clickable(search),
+                            capture_delay=capture_delay)
+            self.find_element(*search).click()
 
-        def run_commands(self, commands):
-            """
-            Run a series of commands. The first item in a command is the
-            browser method to call and the remaining items are the args to
-            pass it.
-            """
-            for method, *args in commands:
-                getattr(self, method)(*args)
+    def click_button(self, substring=''):
+        """
+        Wait for a button with substring to become clickable then click it.
+        """
+        search = (By.XPATH, xpath_contains(f'//button', substring))
+        with self.wrap_exception(f'click button with string "{substring}" when clickable'):
+            wait = Waiter(self, getattr(self, 'default_wait', 5))
+            wait.until(EC.element_to_be_clickable(search))
+            self.find_element(*search).click()
 
-        def snap(self):
-            """Grab a screenshot and store it."""
-            self.pngs.append(self.get_screenshot_as_base64())
+    def wait_for(self, tag, substring, timeout=None, capture_delay=0):
+        """Wait for tag containing substring to show up in the DOM."""
+        if timeout is None:
+            timeout = getattr(self, 'default_wait', 5)
+        search = (By.XPATH, xpath_contains(f'//{tag}', substring))
+        with self.wrap_exception(f'wait for visibility of tag "{tag}" with string "{substring}"'):
+            wait = Waiter(self, timeout)
+            wait.until(EC.visibility_of_element_located(search),
+                        capture_delay=capture_delay)
 
-        def send(self, *strings):
-            """
-            Send the list of strings to the window, with a TAB between each
-            string.
-            """
-            chain = ActionChains(self)
-            chain.send_keys(Keys.TAB.join(strings)).perform()
+    def run_commands(self, commands):
+        """
+        Run a series of commands. The first item in a command is the
+        browser method to call and the remaining items are the args to
+        pass it.
+        """
+        for method, *args in commands:
+            getattr(self, method)(*args)
 
-        def send_inputs(self, *strings):
-            elements = self.find_elements_by_css_selector('input')
-            for element, string in zip(elements, strings):
-                element.send_keys(string)
+    def snap(self):
+        """Grab a screenshot and store it."""
+        self.pngs.append(self.get_screenshot_as_base64())
 
-        def hide_inputs(self):
-            """Obscure all text inputs on the current screen."""
-            javascript = """
-                var inputsToHide = document.querySelectorAll('input');
-                Array.prototype.forEach.call(inputsToHide, function(el){
-                    if (!el.attributes['type'] || el.attributes['type'].value === 'text')
-                        el.setAttribute('type', 'password');
-                })
-            """
-            self.execute_script(javascript)
+    def send(self, *strings):
+        """
+        Send the list of strings to the window, with a TAB between each
+        string.
+        """
+        chain = ActionChains(self)
+        chain.send_keys(Keys.TAB.join(strings)).perform()
 
-        def send_secret(self, *encrypted_strings):
-            """
-            Send the list of strings to the window, decrypting them first.
-            We do this encrypted to keep the secrets from appearing in
-            Tracebacks.
-            """
-            strings = [self.decrypt(string) for string in encrypted_strings]
-            self.send(*strings)
+    def send_inputs(self, *strings):
+        elements = self.find_elements_by_css_selector('input')
+        for element, string in zip(elements, strings):
+            element.send_keys(string)
 
-        def decrypt(self, encrypted_text):
-            """Method to decrypt text to be overridden."""
-            raise NotImplementedError('decrypt should be overridden')
+    def hide_inputs(self):
+        """Obscure all text inputs on the current screen."""
+        javascript = """
+            var inputsToHide = document.querySelectorAll('input');
+            Array.prototype.forEach.call(inputsToHide, function(el){
+                if (!el.attributes['type'] || el.attributes['type'].value === 'text')
+                    el.setAttribute('type', 'password');
+            })
+        """
+        self.execute_script(javascript)
 
-        @contextmanager
-        def wrap_exception(self, message):
-            """Wrap any exceptions caught in a BrowserError with message."""
-            try:
-                yield
-            except Exception as e:
-                if not self.autocapture:
-                    self.snap()
-                raise BrowserError(self, message, str(e))
+    def send_secret(self, *encrypted_strings):
+        """
+        Send the list of strings to the window, decrypting them first.
+        We do this encrypted to keep the secrets from appearing in
+        Tracebacks.
+        """
+        strings = [self.decrypt(string) for string in encrypted_strings]
+        self.send(*strings)
 
-    browser = BrowserRecorder(*args, **kwargs)
-    try:
-        yield browser
-    finally:
-        browser.quit()
+    def decrypt(self, encrypted_text):
+        """Method to decrypt text to be overridden."""
+        raise NotImplementedError('decrypt should be overridden')
+
+    @contextmanager
+    def wrap_exception(self, message):
+        """Wrap any exceptions caught in a BrowserError with message."""
+        try:
+            yield
+        except Exception as e:
+            if not self.autocapture:
+                self.snap()
+            raise BrowserError(self, message, str(e))
 
 
 class Waiter(WebDriverWait):
@@ -192,7 +193,7 @@ class BrowserError(Exception):
 @pytest.fixture(scope='session')
 def browser():
     """Keep a PhantomJS browser open while we run our tests."""
-    with get_browser('node_modules/.bin/phantomjs') as browser:
+    with BrowserRecorder() as browser:
         yield browser
 
 
@@ -341,7 +342,7 @@ def xpath_contains(node, substring):
 
 
 if __name__ == '__main__':
-    with get_browser('node_modules/.bin/phantomjs') as browser:
+    with BrowserRecorder() as browser:
         browser.get('https://github.com/UWIT-IAM/webdriver-recorder')
         browser.wait_for('a', 'webdriver-recorder')
         png = browser.pngs.pop()
