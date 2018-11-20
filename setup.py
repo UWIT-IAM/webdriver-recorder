6c3e821b648d13d3c6b0be797543146173c375b6
Jeff Franklin <jpf@uw.edu>
2018-11-20 15:14:29 -0800

    Add some missing setup arguments raised as warnings.

--- setup.py
+++ setup.py
@@ -2,32 +2,35 @@ import os
 import re
 from setuptools import setup
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-VERSION_FILE = os.path.join(BASE_DIR, 'VERSION')
+VERSION_FILE = os.path.join(BASE_DIR, 'webdriver_recorder', 'VERSION')
 
-if os.path.isfile(VERSION_FILE):
-    # tag written to 'VERSION' via CI.
-    with open(VERSION_FILE) as fd:
-        VERSION = fd.read().strip()
-    VERSION = re.sub('^v', '', VERSION)
-else:
-    VERSION = '0.0.1'
+def strip_comments(line): return line.split('#')[0]
+VERSION = ''.join(map(strip_comments, open(VERSION_FILE).readlines())).strip()
+VERSION = re.sub('^v', '', VERSION)
 
 desc = 'Enhances a selenium webdriver to record screenshots along the way'
-with open(os.path.join(BASE_DIR, 'Readme.md')) as fd:
-    long_description = fd.read()
+long_description = open(os.path.join(BASE_DIR, 'README.md')).read()
 
 
 setup(name='uw-webdriver-recorder',
       install_requires=['selenium', 'pytest', 'jinja2'],
+      url='https://github.com/UWIT-IAM/webdriver-recorder',
+      author='UW-IT Identity and Access Management',
+      author_email='jpf@uw.edu',
       version=VERSION,
       description=desc,
+      license='Apache License, Version 2.0',
       long_description=long_description,
-      long_description_content_type="text/markdown",
       packages=['webdriver_recorder'],
-      package_data={'webdriver_recorder': ['report.template.html']},
+      package_data={'webdriver_recorder': ['VERSION', '*.html']},
       python_requires='>=3.4',
       setup_requires=['pytest-runner'],
       entry_points={'pytest11': [
           'uw-webdriver-recorder = webdriver_recorder.plugin']},
-      classifiers=["Framework :: Pytest"]
+      classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Pytest']
       )
