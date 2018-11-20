9a208d586e833d548c61ec8b04a13f06af3bc579
Jeff Franklin <jpf@uw.edu>
2018-11-19 19:58:38 -0800

    Deploy to PyPI via travis.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(name='uw-webdriver-recorder',
       install_requires=['selenium', 'pytest', 'jinja2'],
-      version='0.5',
+      use_scm_version={"write_to": "webdriver_recorder/_version.py"},
       description=(
           'Enhances a selenium webdriver to record screenshots along the way'),
       packages=['webdriver_recorder'],
       package_data={'webdriver_recorder': ['report.template.html']},
       python_requires='>=3.5',
-      setup_requires=['pytest-runner'],
+      setup_requires=['pytest-runner', 'setuptools_scm'],
       entry_points={'pytest11': [
           'uw-webdriver-recorder = webdriver_recorder.plugin']},
       classifiers=["Framework :: Pytest"]
