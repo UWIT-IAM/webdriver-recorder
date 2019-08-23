1da07cdd350b32488ed49648d60bd6d56e86cc6e
Jeff Franklin <jpf@uw.edu>
2019-08-22 19:26:34 -0700

    Try a more reliable test target.
    Ideally we set up a test server and test against that. However,
    we want a real world example so we test against wiktionary instead.
    Google has started barring scrapers.

--- webdriver_recorder/test_webdriver_recorder.py
+++ webdriver_recorder/test_webdriver_recorder.py
@@ -3,23 +3,13 @@ import pytest
 import time
 
 def test_basic(browser):
-    browser.get('https://google.com')
+    browser.get('https://en.wiktionary.org')
     time.sleep(1)
     browser.snap()
-    browser.send('keyboard cat\n')
-    browser.wait_for('a', 'keyboard cat')
-    browser.click('a', 'images')
-    assert len(browser.pngs) == 3
-
-
-def test_xdist(browser):
-    """pytest-xdist will run many tests in parallel. Test that."""
-    browser.get('https://google.com')
-    time.sleep(1)
-    browser.snap()
-    browser.send('kool aid man\n')
-    browser.wait_for('a', 'kool aid man')
-    browser.click('a', 'images')
+    browser.send_inputs('boundless')
+    browser.send('\n')
+    browser.wait_for('h1', 'boundless')
+    browser.click('a', 'unbounded')
     assert len(browser.pngs) == 3
 
 
