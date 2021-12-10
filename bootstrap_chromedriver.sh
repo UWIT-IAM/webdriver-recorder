6291a4e9b71b92de39c9bdc21fe707d3237175ce
Tom Thorogood <tom@tomthorogood.com>
2021-12-10 15:14:24 -0800

    Auto-detect mac vs. linux when bootstrapping chromedriver

--- bootstrap_chromedriver.sh
+++ bootstrap_chromedriver.sh
@@ -16,19 +16,130 @@
 # To install in /usr/local/bin on linux (a VM/container use case):
 # CHROMEDRIVER_DIR=/usr/local/bin CHROMEDRIVER_DIST=linux64 sudo ./bootstrap_chromedriver.sh
 
+function print_help {
+   cat <<EOF
+   Use: bootstrap_chromedriver.sh [OPTIONS]
+
+   Installs the latest chromedriver instance from Google. If no options are provided,
+   it will be installed to your poetry environment ( if it is set up).
+   You can specify the destination with '-d', if that doesn't suit your needs.
+
+   Options:
+   --dest / -d     The location to store the chromedriver binary.
+                   If not provided, will be installed to the poetry
+                   environment if exists. Otherwise, an error will be raised.
+   --dist          mac64 / linux64 -- If not provided, will be derived from your
+                   environment
+   -h, --help      Show this message and exit
+   -g, --debug     Show commands as they are executing
+EOF
+}
+
+
+# Initialize some globals
+dest=
+dist=
+DEBUG=
+CHROMEDRIVER_VERSION=
+
+function parse_args {
+  while (( $# ))
+  do
+    case $1 in
+      --dest|-d)
+        shift
+        dest="$1"
+        ;;
+      --dist)
+        shift
+        dist="$1"
+        ;;
+      --help|-h)
+        print_help
+        exit 0
+        ;;
+      --debug|-g)
+        DEBUG=1
+        ;;
+      *)
+        echo "Invalid Option: $1"
+        print_help
+        exit 1
+        ;;
+    esac
+    shift
+  done
+}
+function configure_dest {
+  if [[ -z "${dest}" ]]
+  then
+    echo "No destination provided. Checking poetry status."
+    if ! type poetry > /dev/null
+    then
+      echo "No --dest provided, and no poetry installation detected."
+      return 1
+    fi
+    path=$(poetry env list --full-path | tail -n 1 | cut -f1 -d' ')
+    if [[ "$?" != "0" ]] || [[ -z "${path}" ]]
+    then
+      echo "No --dest provided, and no poetry environment exists."
+      return 1
+    fi
+    echo "Installing to poetry environment."
+    dest="${path}/bin"
+  fi
+  if ! [[ -d "${dest}" ]]
+  then
+    echo "Creating destination: ${dest}"
+    mkdir -p ${dest}
+  fi
+}
+
+function configure_dist {
+  if [[ -z "${dist}" ]]
+  then
+    if [[ "$(uname)" == "Darwin" ]]
+    then
+      dist=mac64
+    else
+      dist=linux64
+    fi
+  fi
+}
+
+function chromedriver_version {
+  if [[ -z "${CHROMEDRIVER_VERSION}" ]]
+  then
+    export CHROMEDRIVER_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+  fi
+  echo "${CHROMEDRIVER_VERSION}"
+}
+
+function chromedriver_url {
+  local version="$(chromedriver_version)"
+  local filename="chromedriver_${dist}.zip"
+  echo "https://chromedriver.storage.googleapis.com/${version}/${filename}"
+}
+
+
+function install_chromedriver {
+  local dest_filename="${dest}/chromedriver"
+  local tmp_filename="/tmp/chromedriver.zip"
+
+  echo "Installing chromedriver $(chromedriver_version) for ${dist} to ${dest}"
+  curl -s "$(chromedriver_url)" > "${tmp_filename}"
+  if rm "${dest_filename}" > /dev/null
+  then
+    echo "Removed previous installation of chromedriver from destination"
+  fi
+  unzip "${tmp_filename}" -d "${dest}" > /dev/null
+  chmod 755 "${dest_filename}"
+  rm "${tmp_filename}"
+}
+
+
 set -e
-CHROMEDRIVER_DIR=${CHROMEDRIVER_DIR:-env/bin}
-CHROMEDRIVER_DIST=${CHROMEDRIVER_DIST:-linux64}
-export CHROMEDRIVER_BIN="${CHROMEDRIVER_DIR}/chromedriver"
-export CHROMEDRIVER_VERSION=$(curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-CHROMEDRIVER_URL="https://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromedriver_${CHROMEDRIVER_DIST}.zip"
-
-# Create the destination dir if it does not already exist, and remove any existing chromedriver binaries
-test -d "${CHROMEDRIVER_DIR}" || mkdir -p "${CHROMEDRIVER_DIR}"
-test -f "${CHROMEDRIVER_BIN}" && rm "${CHROMEDRIVER_BIN}"
-
-echo "Installing chromedriver ${CHROMEDRIVER_VERSION} for ${CHROMEDRIVER_DIST} to ${CHROMEDRIVER_DIR}"
-curl "${CHROMEDRIVER_URL}" > /tmp/chromedriver.zip
-unzip /tmp/chromedriver.zip -d "${CHROMEDRIVER_DIR}"
-chmod 755 "${CHROMEDRIVER_DIR}/chromedriver"
-export PATH="${PATH}:${CHROMEDRIVER_DIR}"
+parse_args $@
+configure_dest
+configure_dist
+install_chromedriver
